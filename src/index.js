9e9fcac68ceeff1f5250b6e8b30df5dd0741b50c
formigli <fernando.formigli@columbia.edu.py>
2020-04-25 11:15:47 -0400

    commit inicial iniu

---
+++ src/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+//settings
+app.set('port', 3000);
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+//middlewares
+
+// routes
+app.use(require('./routes/'));
+
+//static files
+app.use(express.static(path.join(__dirname,'public')));
+
+// start server
+app.listen(app.get('port'), () => {
+    console.log('Server on port', app.get('port'));
+});
